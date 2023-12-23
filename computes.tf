995043d375f4a42e02242124b0d81a5a32a51c26
Zack Siri <me@zacksiri.dev>
2023-12-23 13:15:49 +0700

    update ssh-key for antares

--- computes.tf
+++ computes.tf
@@ -12,7 +12,7 @@ module "aws_compute_antares_bcg" {
   network_dependencies = module.aws_networking_antares_uoo.dependencies
   node_size            = "t3a.medium"
   public_subnet_ids    = module.aws_networking_antares_uoo.public_subnet_ids
-  ssh_keys             = ["zack-studio", "zack-mbp"]
+  ssh_keys             = ["zack-studio"]
   storage_size         = 40
   volume_type          = "gp3"
   vpc_id               = module.aws_networking_antares_uoo.vpc_id
